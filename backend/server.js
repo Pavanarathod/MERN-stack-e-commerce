14e6ab8542f15a84d8363175bd96ffd4c8697cbf
Pavan Rathod <edroculllen13@gmail.com>
2021-11-21 13:43:23 +0530

    Mongodb setup and createing models

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./database/mongoose.js";
+import colors from "colors";
+
+connectDB();
 
 dotenv.config();
 const app = express();
@@ -15,5 +19,7 @@ app.get("/api/product/:id", (req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  );
 });
